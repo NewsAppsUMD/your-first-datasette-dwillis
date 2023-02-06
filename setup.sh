cbff597f2cbe61a5a2ae2c84a7693c489b38affe
Derek Willis <dwillis@gmail.com>
2023-02-06 18:11:18 +0000

    added setup and data

---
+++ setup.sh
@@ -0,0 +1,5 @@
+rm -f boe_spending.db
+pip install sqlite-utils datasette
+sqlite-utils insert boe_spending.db vendors boe_spending.csv --csv
+sqlite-utils transform boe_spending.db vendors --type Amount float
+datasette serve boe_spending.db
